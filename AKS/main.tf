55942fb4e895a262f7e0fbb48635c547fcf41395
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-10-28 11:30:18 +0000

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,24 +1,24 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "imagerg"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_container_registry" "ACR" {
-  name                = "containerRegistry17674546"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
+resource "azurerm_container_registry" "example" {
+  name                = "containerRegistry178945"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "myaks2345"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks123"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_D2_v2"
   }
 
@@ -32,8 +32,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.ACR.id
+  scope                            = azurerm_container_registry.example.id
   skip_service_principal_aad_check = true
 }
