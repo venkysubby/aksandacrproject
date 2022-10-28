d9af71107a3a5d5ee907aa4110af74a0993c29af
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-10-28 11:35:49 +0000

    Update provider.tf

--- AKS/provider.tf
+++ AKS/provider.tf
@@ -4,3 +4,12 @@ provider "azurerm" {
   version = "=3.0.0"
   
 }
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "automationpurpose"
+    container_name       = "logs"
+    key                  = "prod.terraform.tfstate"
+    access_key = "C2QxvyaObIQh3oyUEgX5RxbYGTxFojGZnQ/iz7BwMYiWj/QMj3qGXDcYlOt0B8gd6oss8jl4WMn8+AStDNYDvA=="
+  }
+}
