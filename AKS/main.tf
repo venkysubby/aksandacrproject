b0e7f3ee4196b6fb5d3d025b68841220fee285b3
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-10-28 11:11:32 +0000

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_container_registry" "ACR" {
 }
 
 resource "azurerm_kubernetes_cluster" "AKS" {
-  name                = "example-aks1354645"
+  name                = "akscluster1354645"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "exampleaks1"
